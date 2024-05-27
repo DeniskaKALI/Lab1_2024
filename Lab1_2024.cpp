66c1331aa62770411e2d1a4e4172c736973b508e
Deniska <deniskaost2005@gmail.com>
2024-05-27 23:53:34 +0300

    Добавьте файлы проекта.

---
+++ Lab1_2024.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stack>
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(nullptr));
+    std::stack<int> stack;
+    int n;
+    std::cout << "Введите количесво элементов Стека:";
+    std::cin >> n;
+    for (int i = 0; i < n;i++) {
+        stack.push(rand() / 100);
+    }
+    int* p = &stack.top();
+
+    std::cout << "Вершина стека:" << *p << "\n" << "Указатель: " << p;
+
+}
